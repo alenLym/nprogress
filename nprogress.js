9b4f3b83a4f9fd947189a323e395b1b2d6d42f40
alen <592453675@qq.com>
2024-09-02 17:49:52 +0800

    no message

--- nprogress.js
+++ nprogress.js
@@ -1,7 +1,7 @@
 /* NProgress, (c) 2013, 2014 Rico Sta. Cruz - http://ricostacruz.com/nprogress
  * @license MIT */
 
-;(function(root, factory) {
+; (function (root, factory) {
 
   if (typeof define === 'function' && define.amd) {
     define(factory);
@@ -11,7 +11,11 @@
     root.NProgress = factory();
   }
 
-})(this, function() {
+})(this, function () {
+
+
+  // 实例状态 status
+// ---------------------------------------------------------------- instance value 实例属性 -------------------------------
   var NProgress = {};
 
   NProgress.version = '0.2.0';
@@ -30,14 +34,40 @@
     template: '<div class="bar" role="bar"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
   };
 
+
+
+
+
+
+
+
+
+ 
+// ----------------------------------------------------- ---------------------- instance status 实例状态 -------------------------------
+ 
+
+/**
+   * Last number.
+   */
+NProgress.status = null;
+
+
+
+
+
+
+
+  // instance methods 实例方法
+
+// ----------------------------------------------------- ---------------------- instance methods 实例方法 -------------------------------
+
   /**
-   * Updates configuration.
+   * 更新配置。
    *
-   *     NProgress.configure({
-   *       minimum: 0.1
+   *     NProgress.configure（{ 最小值： 0.1
    *     });
    */
-  NProgress.configure = function(options) {
+  NProgress.configure = function (options) {
     var key, value;
     for (key in options) {
       value = options[key];
@@ -47,53 +77,53 @@
     return this;
   };
 
-  /**
-   * Last number.
-   */
 
-  NProgress.status = null;
+
+
+
+
 
   /**
-   * Sets the progress bar status, where `n` is a number from `0.0` to `1.0`.
+   * 设置进度条状态，其中 'n' 是介于 '0.0' 到 '1.0' 之间的数字。
    *
-   *     NProgress.set(0.4);
-   *     NProgress.set(1.0);
+   *     NProgress.set（0.4）;
+   *     NProgress.set（1.0）;
    */
 
-  NProgress.set = function(n) {
+  NProgress.set = function (n) {
     var started = NProgress.isStarted();
 
     n = clamp(n, Settings.minimum, 1);
     NProgress.status = (n === 1 ? null : n);
 
     var progress = NProgress.render(!started),
-        bar      = progress.querySelector(Settings.barSelector),
-        speed    = Settings.speed,
-        ease     = Settings.easing;
+      bar = progress.querySelector(Settings.barSelector),
+      speed = Settings.speed,
+      ease = Settings.easing;
 
     progress.offsetWidth; /* Repaint */
 
-    queue(function(next) {
-      // Set positionUsing if it hasn't already been set
+    queue(function (next) {
+      // 如果尚未设置 positionUsing，则设置
       if (Settings.positionUsing === '') Settings.positionUsing = NProgress.getPositioningCSS();
 
-      // Add transition
+      // 添加过渡
       css(bar, barPositionCSS(n, speed, ease));
 
       if (n === 1) {
-        // Fade out
+        // 淡出
         css(progress, {
           transition: 'none',
           opacity: 1
         });
         progress.offsetWidth; /* Repaint */
 
-        setTimeout(function() {
+        setTimeout(function () {
           css(progress, {
             transition: 'all ' + speed + 'ms linear',
             opacity: 0
           });
-          setTimeout(function() {
+          setTimeout(function () {
             NProgress.remove();
             next();
           }, speed);
@@ -106,22 +136,22 @@
     return this;
   };
 
-  NProgress.isStarted = function() {
+  NProgress.isStarted = function () {
     return typeof NProgress.status === 'number';
   };
 
   /**
-   * Shows the progress bar.
-   * This is the same as setting the status to 0%, except that it doesn't go backwards.
+   * 显示进度条。
+   * 这与将 status 设置为 0% 相同，只是它不会倒退。
    *
-   *     NProgress.start();
+   *     NProgress.start（）;
    *
    */
-  NProgress.start = function() {
+  NProgress.start = function () {
     if (!NProgress.status) NProgress.set(0);
 
-    var work = function() {
-      setTimeout(function() {
+    var work = function () {
+      setTimeout(function () {
         if (!NProgress.status) return;
         NProgress.trickle();
         work();
@@ -134,33 +164,32 @@
   };
 
   /**
-   * Hides the progress bar.
-   * This is the *sort of* the same as setting the status to 100%, with the
-   * difference being `done()` makes some placebo effect of some realistic motion.
+   * 隐藏进度条。
+   * 这与将状态设置为 100% 相同，区别在于 'done（）' 会产生一些现实运动的安慰剂效应。
    *
-   *     NProgress.done();
+   *     NProgress.done（）;
    *
-   * If `true` is passed, it will show the progress bar even if its hidden.
+   * 如果传递了 'true'，则即使进度条被隐藏，它也会显示进度条。
    *
-   *     NProgress.done(true);
+   *     NProgress.done（真）;
    */
 
-  NProgress.done = function(force) {
+  NProgress.done = function (force) {
     if (!force && !NProgress.status) return this;
 
     return NProgress.inc(0.3 + 0.5 * Math.random()).set(1);
   };
 
   /**
-   * Increments by a random amount.
+   * 按随机量递增。
    */
 
-  NProgress.inc = function(amount) {
+  NProgress.inc = function (amount) {
     var n = NProgress.status;
 
     if (!n) {
       return NProgress.start();
-    } else if(n > 1) {
+    } else if (n > 1) {
       return;
     } else {
       if (typeof amount !== 'number') {
@@ -176,20 +205,19 @@
     }
   };
 
-  NProgress.trickle = function() {
+  NProgress.trickle = function () {
     return NProgress.inc();
   };
 
   /**
-   * Waits for all supplied jQuery promises and
-   * increases the progress as the promises resolve.
+   * 等待所有提供的 jQuery Promise，并在 Promise 解析时增加进度。
    *
    * @param $promise jQUery Promise
    */
-  (function() {
+  (function () {
     var initial = 0, current = 0;
 
-    NProgress.promise = function($promise) {
+    NProgress.promise = function ($promise) {
       if (!$promise || $promise.state() === "resolved") {
         return this;
       }
@@ -201,13 +229,13 @@
       initial++;
       current++;
 
-      $promise.always(function() {
+      $promise.always(function () {
         current--;
         if (current === 0) {
-            initial = 0;
-            NProgress.done();
+          initial = 0;
+          NProgress.done();
         } else {
-            NProgress.set((initial - current) / initial);
+          NProgress.set((initial - current) / initial);
         }
       });
 
@@ -217,11 +245,11 @@
   })();
 
   /**
-   * (Internal) renders the progress bar markup based on the `template`
-   * setting.
+   * （内部）根据 'template' 呈现进度条标记
+   * 设置。
    */
 
-  NProgress.render = function(fromStart) {
+  NProgress.render = function (fromStart) {
     if (NProgress.isRendered()) return document.getElementById('nprogress');
 
     addClass(document.documentElement, 'nprogress-busy');
@@ -233,11 +261,11 @@
 
 
     var bar = progress.querySelector(Settings.barSelector),
-        perc = fromStart ? '-100' : toBarPerc(NProgress.status || 0),
-        parent = isDOM(Settings.parent)
-          ? Settings.parent
-          : document.querySelector(Settings.parent),
-        spinner
+      perc = fromStart ? '-100' : toBarPerc(NProgress.status || 0),
+      parent = isDOM(Settings.parent)
+        ? Settings.parent
+        : document.querySelector(Settings.parent),
+      spinner
 
     css(bar, {
       transition: 'all 0 linear',
@@ -258,10 +286,10 @@
   };
 
   /**
-   * Removes the element. Opposite of render().
+   * 删除元素。与 render（） 相反。
    */
 
-  NProgress.remove = function() {
+  NProgress.remove = function () {
     removeClass(document.documentElement, 'nprogress-busy');
     var parent = isDOM(Settings.parent)
       ? Settings.parent
@@ -272,44 +300,57 @@
   };
 
   /**
-   * Checks if the progress bar is rendered.
+   * 检查进度条是否已呈现。
    */
 
-  NProgress.isRendered = function() {
+  NProgress.isRendered = function () {
     return !!document.getElementById('nprogress');
   };
 
   /**
-   * Determine which positioning CSS rule to use.
+   * 确定要使用的定位 CSS 规则。
    */
 
-  NProgress.getPositioningCSS = function() {
-    // Sniff on document.body.style
+  NProgress.getPositioningCSS = function () {
+    // 嗅探 document.body.style
     var bodyStyle = document.body.style;
 
-    // Sniff prefixes
+    // 嗅探前缀
     var vendorPrefix = ('WebkitTransform' in bodyStyle) ? 'Webkit' :
-                       ('MozTransform' in bodyStyle) ? 'Moz' :
-                       ('msTransform' in bodyStyle) ? 'ms' :
-                       ('OTransform' in bodyStyle) ? 'O' : '';
+      ('MozTransform' in bodyStyle) ? 'Moz' :
+        ('msTransform' in bodyStyle) ? 'ms' :
+          ('OTransform' in bodyStyle) ? 'O' : '';
 
     if (vendorPrefix + 'Perspective' in bodyStyle) {
-      // Modern browsers with 3D support, e.g. Webkit, IE10
+      // 支持 3D 的现代浏览器，例如 Webkit、IE10
       return 'translate3d';
     } else if (vendorPrefix + 'Transform' in bodyStyle) {
-      // Browsers without 3D support, e.g. IE9
+      // 不支持 3D 的浏览器，例如 IE9
       return 'translate';
     } else {
-      // Browsers without translate() support, e.g. IE7-8
+      // 不支持 translate（） 的浏览器，例如 IE7-8
       return 'margin';
     }
   };
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------ helper functions 辅助函数---------------------------------------------
+
+
+  
 
   /**
    * Helpers
    */
 
-  function isDOM (obj) {
+  function isDOM(obj) {
     if (typeof HTMLElement === 'object') {
       return obj instanceof HTMLElement
     }
@@ -321,6 +362,20 @@
     )
   }
 
+  /**
+   * 'clamp' 函数确保给定的数字 'n' 保持在由 'min' 定义的指定范围内，并且
+   * 'max' 值。
+   * @param n - “clamp”函数中的参数“n”表示你想要的数字
+   * 在由 'min' 和 'max' 值定义的指定范围内进行约束。
+   * @param min - “clamp”函数中的“min”参数表示输入
+   * 'n' 可以是。如果 'n' 小于这个 'min' 值，函数将返回 'min' 而不是 'n'。
+   * @param max - “clamp”函数中的“max”参数表示输入的最大值
+   * 'n' 可以是。如果 'n' 大于这个 'max' 值，该函数将返回 'max'。
+   * @returns 如果 'n' 的值小于 'min' 值，则返回 'min'。如果
+   * 'n' 的值大于 'max' 值，则返回 'max'。否则，将
+   * 将返回 'n'。
+   */
+
   function clamp(n, min, max) {
     if (n < min) return min;
     if (n > max) return max;
@@ -328,8 +383,8 @@
   }
 
   /**
-   * (Internal) converts a percentage (`0..1`) to a bar translateX
-   * percentage (`-100%..0%`).
+   * （内部）将百分比 （'0..1'） 转换为条形 translateX
+   * 百分比 （'-100%..0%'）。
    */
 
   function toBarPerc(n) {
@@ -338,31 +393,31 @@
 
 
   /**
-   * (Internal) returns the correct CSS for changing the bar's
-   * position given an n percentage, and speed and ease from Settings
+   * （Internal） 返回正确的 CSS 以更改栏的
+   * position 给定 n 百分比，以及 Settings 中的 Speed 和 ease
    */
 
   function barPositionCSS(n, speed, ease) {
     var barCSS;
 
     if (Settings.positionUsing === 'translate3d') {
-      barCSS = { transform: 'translate3d('+toBarPerc(n)+'%,0,0)' };
+      barCSS = { transform: 'translate3d(' + toBarPerc(n) + '%,0,0)' };
     } else if (Settings.positionUsing === 'translate') {
-      barCSS = { transform: 'translate('+toBarPerc(n)+'%,0)' };
+      barCSS = { transform: 'translate(' + toBarPerc(n) + '%,0)' };
     } else {
-      barCSS = { 'margin-left': toBarPerc(n)+'%' };
+      barCSS = { 'margin-left': toBarPerc(n) + '%' };
     }
 
-    barCSS.transition = 'all '+speed+'ms '+ease;
+    barCSS.transition = 'all ' + speed + 'ms ' + ease;
 
     return barCSS;
   }
 
   /**
-   * (Internal) Queues a function to be executed.
+   * （内部）将要执行的函数排队。
    */
 
-  var queue = (function() {
+  var queue = (function () {
     var pending = [];
 
     function next() {
@@ -372,26 +427,25 @@
       }
     }
 
-    return function(fn) {
+    return function (fn) {
       pending.push(fn);
       if (pending.length == 1) next();
     };
   })();
 
   /**
-   * (Internal) Applies css properties to an element, similar to the jQuery
-   * css method.
+   * （内部）将 css 属性应用于元素，类似于 jQuery
+   * CSS 方法。
    *
-   * While this helper does assist with vendor prefixed property names, it
-   * does not perform any manipulation of values prior to setting styles.
+   * 虽然此帮助程序确实有助于使用供应商前缀的属性名称，但它不会在设置样式之前对值执行任何操作。
    */
 
-  var css = (function() {
-    var cssPrefixes = [ 'Webkit', 'O', 'Moz', 'ms' ],
-        cssProps    = {};
+  var css = (function () {
+    var cssPrefixes = ['Webkit', 'O', 'Moz', 'ms'],
+      cssProps = {};
 
     function camelCase(string) {
-      return string.replace(/^-ms-/, 'ms-').replace(/-([\da-z])/gi, function(match, letter) {
+      return string.replace(/^-ms-/, 'ms-').replace(/-([\da-z])/gi, function (match, letter) {
         return letter.toUpperCase();
       });
     }
@@ -401,8 +455,8 @@
       if (name in style) return name;
 
       var i = cssPrefixes.length,
-          capName = name.charAt(0).toUpperCase() + name.slice(1),
-          vendorName;
+        capName = name.charAt(0).toUpperCase() + name.slice(1),
+        vendorName;
       while (i--) {
         vendorName = cssPrefixes[i] + capName;
         if (vendorName in style) return vendorName;
@@ -421,10 +475,10 @@
       element.style[prop] = value;
     }
 
-    return function(element, properties) {
+    return function (element, properties) {
       var args = arguments,
-          prop,
-          value;
+        prop,
+        value;
 
       if (args.length == 2) {
         for (prop in properties) {
@@ -438,7 +492,7 @@
   })();
 
   /**
-   * (Internal) Determines if an element or space separated list of class names contains a class name.
+   * （内部）确定元素或以空格分隔的类名列表是否包含类名。
    */
 
   function hasClass(element, name) {
@@ -447,40 +501,39 @@
   }
 
   /**
-   * (Internal) Adds a class to an element.
+   * （内部）向元素添加类。
    */
 
   function addClass(element, name) {
     var oldList = classList(element),
-        newList = oldList + name;
+      newList = oldList + name;
 
     if (hasClass(oldList, name)) return;
 
-    // Trim the opening space.
+    // 修剪开口空间。
     element.className = newList.substring(1);
   }
 
   /**
-   * (Internal) Removes a class from an element.
+   * （内部）从元素中删除类。
    */
 
   function removeClass(element, name) {
     var oldList = classList(element),
-        newList;
+      newList;
 
     if (!hasClass(element, name)) return;
 
-    // Replace the class name.
+    // 替换类名。
     newList = oldList.replace(' ' + name + ' ', ' ');
 
-    // Trim the opening and closing spaces.
+    // 修剪开始和结束空间。
     element.className = newList.substring(1, newList.length - 1);
   }
 
   /**
-   * (Internal) Gets a space separated list of the class names on the element.
-   * The list is wrapped with a single space on each end to facilitate finding
-   * matches within the list.
+   * （内部）获取元素上类名称的空格分隔列表。
+   * 该列表的两端各用一个空格括起来，以便于在列表中查找匹配项。
    */
 
   function classList(element) {
@@ -488,7 +541,7 @@
   }
 
   /**
-   * (Internal) Removes an element from the DOM.
+   * （内部）从 DOM 中删除元素。
    */
 
   function removeElement(element) {
